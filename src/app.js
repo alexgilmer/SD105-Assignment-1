67ba64d6c6d3916c4abb03356d160082ef6cbd55
Alex Gilmer <alexgilmer@student.mitt.ca>
2021-05-17 12:09:49 -0500

    Set up getStops function

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
+const baseAPIString = 'https://api.winnipegtransit.com/v3/';
 const myAPIKey = 'Pwv2RRw9obTuNAXMOwK8';
 
 const getStreets = async function(string) {
-  const response = await fetch(`https://api.winnipegtransit.com/v3/streets.json?api-key=${myAPIKey}&name=${string}`)
+  const response = await fetch(`${baseAPIString}streets.json?api-key=${myAPIKey}&name=${string}`)
   const data = await response.json();
   return data.streets;
 };
 
+const getStops = async function(streetKey) {
+  const response = await fetch(`${baseAPIString}stops.json?api-key=${myAPIKey}&street=${streetKey}`);
+  const data = await response.json();
+  return data.stops;
+};
 
-getStreets('main').then(data => console.log(data));
-
+getStreets('main')
+.then((data) => {
+  return getStops(data[0].key)
+})
+.then((data) => {
+  console.log(data)
+})
+.catch(err => {
+  console.log('Something went wrong:');
+  console.log(err);
+})
