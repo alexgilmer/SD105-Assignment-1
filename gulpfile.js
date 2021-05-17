cc5c3c2c6062f00acd58bd5ceb568839c5ab39b8
Alex Gilmer <alexgilmer@student.mitt.ca>
2021-05-17 11:11:55 -0500

    Add htmlreplace, concat, sourcemaps, cleanCSS, and uglify to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,30 @@
 const {src, dest, series} = require('gulp');
+const htmlReplace = require('gulp-html-replace');
+const concat = require('gulp-concat');
+const {init, write} = require('gulp-sourcemaps');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
 
 const htmlTask = function() {
   return src('src/*.html')
+    .pipe(htmlReplace({css: 'all-styles.css'}))
     .pipe(dest('dist/'));
 };
 
 const styleTask = function() {
   return src('src/*.css')
+    .pipe(init())
+      .pipe(concat('all-styles.css'))
+      .pipe(cleanCSS())
+    .pipe(write())
     .pipe(dest('dist/'));
 };
 
 const jsTask = function() {
   return src('src/*.js')
+    .pipe(init())
+      .pipe(uglify())
+    .pipe(write())
     .pipe(dest('dist/'));
 };
 
